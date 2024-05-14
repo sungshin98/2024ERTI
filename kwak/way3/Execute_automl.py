2e5c20559d6e2c5a3c8bbafe1bffcd1d9ddc3347
sungshin98 <130137059+sungshin98@users.noreply.github.com>
2024-05-14 18:29:27 +0900

    2024ETRI

--- kwak/way3/Execute_automl.py
+++ kwak/way3/Execute_automl.py
@@ -7,6 +7,7 @@ from pycaret.datasets import get_data
 from pycaret.classification import *
 import optuna
 import preproc_data
+
 data = preproc_data.merged_data
 clf = setup(data, target='S4', session_id=123, use_gpu=True)
 # Compare models
