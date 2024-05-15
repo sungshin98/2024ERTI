e6a8654ced7a830e5255cf8365de64670dd2da4f
sungshin98 <130137059+sungshin98@users.noreply.github.com>
2024-05-15 20:52:16 +0900

    asd

---
+++ kim/S2_way1/file_name.py
@@ -0,0 +1,11 @@
+from os import path
+
+data_path = r"D:\dataset"
+
+file_path = path.join(data_path,"user_sleep_2020.csv")
+
+clustering_path = path.join(data_path,"created_data", "clusted_info.csv")
+
+survey_reason_path = path.join(data_path, "created_data", "survey_reason.csv")
+
+merge_path = path.join(data_path,"created_data", "merged_df.csv")
